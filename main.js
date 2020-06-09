965672cdbaf66a8931f59cc83625c13dc71533ec
Gianpiero <gpi91mx@gmail.com>
2020-06-09 17:45:51 +0200

    "Frustra fit per plura quod fieri potest per pauciora"

--- main.js
+++ main.js
@@ -24,15 +24,9 @@ function clickNext() {
   if (currentImage.hasClass('last')){
     $('.images img.first').addClass('active');
     $('.nav i.first').addClass('active');
-    $('.nav i').removeClass('red');
-    $('.nav i.first').addClass('red');
-
   } else {
     currentImage.next('img').addClass('active');
     currentSpot.next('i').addClass('active');
-    currentSpot.next('i').addClass('red');
-    currentSpot.prev('i').removeClass('red');
-
   }
 }
 // CREO FUNZIONE PER PASSARE ALL'IMMAGINE PRECEDENTE
@@ -46,12 +40,8 @@ function clickPrev() {
   if (currentImage.hasClass('first')){
     $('.images img.last').addClass('active');
     $('.nav i.last').addClass('active');
-    $('.nav i').removeClass('red');
-    $('.nav i.last').addClass('red');
   } else {
     currentImage.prev('img').addClass('active');
     currentSpot.prev('i').addClass('active');
-    currentSpot.next('i').removeClass('red');
-    currentSpot.prev('i').addClass('red');
   }
 }
